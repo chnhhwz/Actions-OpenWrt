93050b25ceff46cead9bdf35a9ec854eae0d9769
chnhhwz <103352599+chnhhwz@users.noreply.github.com>
2024-04-26 16:35:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,11 +10,18 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
+# 添加SSRplus插件
+# 使用Lean源
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 使用其他源
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-# Add a feed source
-git clone https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
-# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# 添加OpenClash插件
+echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+
+# 添加PassWall插件
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+# 添加kenzok8插件集合（包含SSRplus、OpenClash、PassWall）
 # echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
