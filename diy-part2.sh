90babe71be693efbb65d699dbe2cc31a8bd22c4b
chnhhwz <103352599+chnhhwz@users.noreply.github.com>
2025-01-08 15:42:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,7 +15,7 @@
 sed -i 's/192.168.1.1/192.168.100.10/g' package/base-files/files/bin/config_generate
 
 # 修改版本信息
-sed -i "s/OpenWrt /Z-turn $(TZ=UTC-8 date "+%Y") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /Z-turn $(TZ=UTC-8 date "+%Y.%m") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改主机名
 sed -i 's/OpenWrt/Go-Belie/g' package/base-files/files/bin/config_generate
@@ -24,15 +24,15 @@ sed -i 's/OpenWrt/Go-Belie/g' package/base-files/files/bin/config_generate
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 
 # 安装MultiSD_Lite
-rm -rf feeds/packages/net/msd_lite
-git clone https://github.com/ximiTech/msd_lite.git feeds/packages/net/msd_lite
+#rm -rf feeds/packages/net/msd_lite
+#git clone https://github.com/ximiTech/msd_lite.git feeds/packages/net/msd_lite
 
 # 安装argon主题 (使用kenzok8插件集合仓库时不用添加)
-rm -rf feeds/luci/themes/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+#rm -rf feeds/luci/themes/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
 
-rm -rf feeds/luci/applications/luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
+#rm -rf feeds/luci/applications/luci-app-argon-config
+#git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
 
 # 删除可能冲突的插件（使用kenzok8插件集合仓库时）
 # rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
