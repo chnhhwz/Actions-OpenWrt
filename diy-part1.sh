583f779d50d977d9d0e8cd5ec06f01e70b6f1ca2
chnhhwz <103352599+chnhhwz@users.noreply.github.com>
2025-01-09 17:00:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,3 @@ sed -i '/^src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23\
 # 添加PassWall插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-
-# 添加kenzok8插件集合（包含SSRplus、OpenClash、PassWall）
-#echo 'src-git smpackage https://github.com/kenzok8/small-package' >>feeds.conf.default
