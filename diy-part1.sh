faa7bf2d4fa96a38ef70aa09dafe4e3d951c2550
chnhhwz <103352599+chnhhwz@users.noreply.github.com>
2025-01-11 00:06:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,5 +6,5 @@ sed -i '/^src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23\
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 
 # 添加PassWall插件
-#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-#echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
